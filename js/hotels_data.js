dbe60ea940f99939f8423f340d9ff856b9bc416d
Samantha Stidd <desertdiscordia@gmail.com>
2025-11-29 21:34:50 -0700

    test1

---
+++ js/hotels_data.js
@@ -0,0 +1,92 @@
+export const hotels = [
+  {
+    "image": "images/hotel1.webp",
+    "name": "Sunset Bay Resort",
+    "phone": "555-321-7788",
+    "address": [
+      "123 Oceanview Drive",
+      "Seabreeze, CA 90210"
+    ]
+  },
+  {
+    "image": "images/hotel2.webp",
+    "name": "Mountain Crest Lodge",
+    "phone": "555-874-6621",
+    "address": [
+      "88 Pine Summit Road",
+      "Highland, CO 80439"
+    ]
+  },
+  {
+    "image": "images/hotel3.webp",
+    "name": "Lakeside Haven Inn",
+    "phone": "555-290-4472",
+    "address": [
+      "45 Lakefront Circle",
+      "Clearwater, MN 55320"
+    ]
+  },
+  {
+    "image": "images/hotel4.webp",
+    "name": "Grand Maple Hotel",
+    "phone": "555-669-1183",
+    "address": [
+      "501 Maple Avenue",
+      "Burlington, VT 05401"
+    ]
+  },
+  {
+    "image": "images/hotel5.webp",
+    "name": "Harborview Suites",
+    "phone": "555-902-1138",
+    "address": [
+      "700 Marina Parkway",
+      "Portside, OR 97365"
+    ]
+  },
+  {
+    "image": "images/hotel6.webp",
+    "name": "Desert Star Retreat",
+    "phone": "555-447-9910",
+    "address": [
+      "222 Sand Dune Drive",
+      "Mesa Vista, AZ 85201"
+    ]
+  },
+  {
+    "image": "images/hotel7.webp",
+    "name": "Evergreen Valley Inn",
+    "phone": "555-784-6234",
+    "address": [
+      "18 Forest Ridge Lane",
+      "Pinewood, WA 98101"
+    ]
+  },
+  {
+    "image": "images/hotel8.webp",
+    "name": "Cityscape Towers Hotel",
+    "phone": "555-113-5528",
+    "address": [
+      "940 Main Street",
+      "Metroview, IL 60601"
+    ]
+  },
+  {
+    "image": "images/hotel9.webp",
+    "name": "Riverbend Boutique Hotel",
+    "phone": "555-776-2049",
+    "address": [
+      "302 Riverside Drive",
+      "Bendwell, KY 41011"
+    ]
+  },
+  {
+    "image": "images/hotel10.webp",
+    "name": "Crescent Moon Inn",
+    "phone": "555-662-9182",
+    "address": [
+      "77 Crescent Lane",
+      "Moonrise, NM 87501"
+    ]
+  }
+]
---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+const hb = document.querySelector('#hamburgerBtn');
+const pw = document.querySelector('#pageWrapper');
+
+hb.addEventListener('click', () => {
+    pw.classList.toggle('moveOver');
+});
+
+// grab the hotel location data
+import { hotels } from "./hotels_data.js";
+
+const hotelCards = document.querySelector('#cards');
+
+hotels.forEach(hotel => {
+    const mySection = document.createElement('section');
+
+    const theImage = document.createElement('img');
+    theImage.src = `images/${hotel.photo}`;
+    theImage.alt = hotel.name;
+
+    const theName = document.createElement('h2');
+    theName.textContent = hotel.name;
+
+    const thePhone = document.createElement('a');
+    thePhone.textContent = hotel.phone;
+    thePhone.href = `tel:${hotel.phone}`;
+
+    const theAddress = document.createElement('address');
+    theAddress.innerHTML = `${hotel.address[0]}<br>${hotel.address[1]}`;
+
+    mySection.appendChild(theImage);
+    mySection.appendChild(theName);
+    mySection.appendChild(thePhone);
+    mySection.appendChild(theAddress);
+
+    hotelCards.appendChild(mySection);
+});
